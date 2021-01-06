55bc0a0c782747f10e8b22f720b5d1f995a8d515
Antoine Feuillet <antoine.feuillet@sap.com>
2021-01-06 22:31:18 +0100

    Euler 038

---
+++ Euler/038.py
@@ -0,0 +1,19 @@
+from utils import *
+
+maxp = 0
+def pandmul(nb):
+	stri = ''
+	#import pdb;pdb.set_trace()
+	for j in range(1, 9):
+		stri += str(nb * j)
+		if len(stri) > 9:
+			return False
+		elif len(stri) == 9 and is_pandigital(stri):
+			return stri
+
+for i in range(9999):
+	pandi = pandmul(i)
+	if pandi:
+		print("Pandigital multiple: %s" % (i))
+		maxp = max(maxp, pandi)
+print("Max pandigital: %s" % (maxp))
