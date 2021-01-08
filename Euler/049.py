0f9186ef5ff796ea9e5019a45d5ab1b03d9fea59
Antoine Feuillet <antoine.feuillet@sap.com>
2021-01-08 22:52:54 +0100

    Euler 049

--- Euler/049.py
+++ Euler/049.py
@@ -1,1 +1,11 @@
 from utils import *
+
+
+for i in range(1000, 9999):
+	for j in range(1, 8999):
+		if i + j * 2 > 9999:
+			continue
+		if (sorted(str(i)) == sorted(str(i + j)) and 
+			sorted(str(i)) == sorted(str(i + j * 2)) and 
+			is_prime(i) and is_prime(i + j) and is_prime(i + j * 2)):
+			print("Three prime permutations: %s%s%s + %s" % (i, i + j, i + j*2, j))
