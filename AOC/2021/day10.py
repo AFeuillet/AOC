97d5924b84d965a1809e3b2be7b6667b4f457e51
Antoine Feuillet <antoine.feuillet@sap.com>
2021-12-10 11:50:51 +0100

    AOC 2021 Day 10 Cleaning

--- AOC/2021/day10.py
+++ AOC/2021/day10.py
@@ -3,41 +3,29 @@ nbl = nfile.read().split('\n')
 
 total_part1 = 0
 total2 = []
+closes = {
+	'}': {'value': 1197, 'op': '{'},
+	')': {'value': 3, 'op': '('},
+	']': {'value': 57, 'op': '['},
+	'>': {'value': 25137, 'op': '<'}
+	}
+opens = {'{': 3, '(': 1, '[': 2, '<': 4}
 for line in nbl:
 	total_part2 = 0
 	opener = []
 	for i, c in enumerate(line):
-		if c in ['{', '[', '<', '(']:
+		if c in opens.keys():
 			opener.append(c)
 		else:
-			if (
-				(c == '}' and opener[-1] == '{') or
-				(c == ')' and opener[-1] == '(') or
-				(c == ']' and opener[-1] == '[') or 
-				(c == '>' and opener[-1] == '<')
-				):
+			if c in closes.keys() and opener[-1] == closes[c]['op']:
 				opener.pop()
 			else:
-				if c == '}':
-					total_part1 += 1197
-				elif c == ')':
-					total_part1 += 3
-				elif c == ']':
-					total_part1 += 57
-				elif c == '>':
-					total_part1 += 25137
+				total_part1 += closes[c]['value']
 				break
 		if i == len(line) - 1:
 			for o in reversed(opener):
 				total_part2 *= 5
-				if o == '{':
-					total_part2 += 3
-				elif o == '(':
-					total_part2 += 1
-				elif o == '[':
-					total_part2 += 2
-				elif o == '<':
-					total_part2 += 4
+				total_part2 += opens[o]
 			total2.append(total_part2)
 
 print("Part One: %s" % total_part1)
