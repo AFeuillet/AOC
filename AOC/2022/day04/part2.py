df0f51733dfc389e70fabc66fa18a063ae2a418d
Antoine Feuillet <antoine.feuillet@sap.com>
2022-12-05 07:45:05 +0100

    AOC 2022 Day04

---
+++ AOC/2022/day04/part2.py
@@ -0,0 +1,14 @@
+nbl = open('data.txt').read().split('\n')
+
+total = 0
+for nb in nbl:
+	elves = nb.split(',')
+	elve1 = elves[0].split('-')
+	elve2 = elves[1].split('-')
+	if ((int(elve1[0]) >= int(elve2[0]) and int(elve1[0]) <= int(elve2[1])) or 
+		(int(elve1[1]) >= int(elve2[0]) and int(elve1[1]) <= int(elve2[1])) or 
+		(int(elve1[0]) >= int(elve2[0]) and int(elve1[1]) <= int(elve2[1])) or 
+		(int(elve1[0]) <= int(elve2[0]) and int(elve1[1]) >= int(elve2[1]))):
+		total += 1
+
+print('Part 2: {}'.format(total))
