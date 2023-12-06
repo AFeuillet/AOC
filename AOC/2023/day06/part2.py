d114cd6eeca925240aed9378c4d31e0c61498824
Antoine Feuillet <antoine.feuillet@sap.com>
2023-12-06 13:51:37 +0100

    AOC 2023 day 6

---
+++ AOC/2023/day06/part2.py
@@ -0,0 +1,22 @@
+import re
+
+nfile = open('data.txt', 'r')
+nbl = nfile.read().split('\n')
+
+times = [int(''.join(re.findall(r'(\d+)', nbl[0])))]
+records = [int(''.join(re.findall(r'(\d+)', nbl[1])))]
+
+total = 0
+bigtotal = 1
+for k, tim in enumerate(times):
+	status = False
+	total = 0
+	for trry in range(records[k]):
+		if (tim * trry - trry ** 2) > records[k]:
+			status = True
+			total += 1
+		elif status == True:
+			bigtotal *= total
+			break
+
+print(bigtotal)
